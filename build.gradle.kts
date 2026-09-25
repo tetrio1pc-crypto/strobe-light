85bb931e1593c69e6d381c4bca10c8d75153fca0
tetrio1pc-crypto <tetrio1pc@gmail.com>
2026-09-25 17:41:15 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,22 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.example.strobelight"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.strobelight"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
 }
